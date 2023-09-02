102c46a98e00d35e276d48dec638431804e06680
tryptech <yo.tryptech@gmail.com>
2023-09-01 20:47:12 -0400

    Save and load settings from json file

--- zoom_and_follow_mouse.py
+++ zoom_and_follow_mouse.py
@@ -1,7 +1,8 @@
-from json import loads
+
 from math import sqrt
 from platform import system
 import os
+import json
 import pywinctl as pwc
 import obspython as obs
 
@@ -73,7 +74,8 @@ class ZoomSettings:
     file_name = ""
     file_path = ""
 
-    def __init__(self):
+    def __init__(self, cwd, settings_dir, settings_file_name):
+        log("Run ZoomSettings init")
         self.file_dir = os.path.join(cwd,settings_dir)
         self.file_name = settings_file_name
         self.file_path = os.path.join(self.file_dir,self.file_name)
@@ -86,23 +88,30 @@ class ZoomSettings:
                 os.makedirs(self.file_dir)
             log("Settings directory found")
 
-    def save(self):
+    def save(self, settings):
         try:
-            log("a")
+            log(f"Saving to {self.file_path}")
+            f = open(self.file_path, "w" if os.path.exists(self.file_path) else "a")
+            f.write(str(json.dumps(
+                json.loads(obs.obs_data_get_json(settings)),
+                sort_keys=True,
+                indent=4
+            )))
+            f.close()
         except Exception as e:
             print(e, "Cannot write settings to file")
 
     def load(self):
         try:
-            log("a")
+            log("Loading settings")
+            if not os.path.exists(self.file_path):
+                self.create()
+            f = open(self.file_path)
+            d = json.load(f)
+            f.close()
+            return d
         except Exception as e:
             print(e, "Cannot load settings from file")
-
-    def create(self):
-        if not os.path.exists(self.file_path):
-            log("Making new settings file")
-        else:
-            log("Settings file already exists")
 
 
 # -------------------------------------------------------------------
@@ -153,15 +162,88 @@ SOURCES = CaptureSources(
 class CursorWindow:
     log("Create CursorWindow")
 
+    lock = False  # Activate zoom mode?
+    track = True  # Follow mouse cursor while in zoom mode?
+    update = True  # Animating between zoom in and out?
+    ticking = False  # To prevent subscribing to timer multiple times
+    zi_timer = zo_timer = 0  # Frames spent on zoom in/out animations
+    windows = window_titles = monitor = window = window_handle \
+        = window_name = ''
+    monitors = pwc.getAllScreens()
+    monitors_key = list(dict.keys(monitors))
+    monitor_override = manual_offset = monitor_size_override = False
+    monitor_override_id = ''
+    zoom_x = zoom_y = 0  # Zoomed-in window top left location
+    zoom_x_target = zoom_y_target = 0  # Interpolate the above towards these
+    # Actual source (window or monitor) location and dimensions from the system
+    source_w_raw = source_h_raw = source_x_raw = source_y_raw = 0
+    # Overriden source location and dimensions from settings
+    source_x_offset = source_y_offset \
+        = source_w_override = source_h_override = 0
+    # Computed source location and dimensions that depend on whether override
+    # settings are enabled.
+    source_x = source_y = source_w = source_h = 0
+    source_load = False
+    refresh_rate = 16
+    source_name = source_type = ''
+    zoom_w = 1280
+    zoom_h = 720
+    active_border = 0.15
+    max_speed = 160
+    smooth = 1.0
+    zoom_time = 300
+
+    def update_sources(self, settings_update = False):
+        """
+        Update the list of Windows and Monitors from PyWinCtl
+        """
+        if not (sys == "Darwin") or not settings_update:
+            self.windows = pwc.getAllWindows()
+            self.monitors = pwc.getAllScreens()
+            self.monitors_key = list(dict.keys(self.monitors))
+
 
 # -------------------------------------------------------------------
 class Callbacks:
     log("Create Callbacks")
 
+
 # -------------------------------------------------------------------
-zs = ZoomSettings()
+zs = ZoomSettings(cwd, settings_dir, settings_file_name)
 zoom = CursorWindow()
 cb = Callbacks()
+
+
+# -------------------------------------------------------------------
+def populate_list_property_with_source_names(list_property):
+    """
+    Updates Zoom Source's available options.
+
+    Checks a source against SOURCES to determine availability.
+    """
+    global new_source
+
+    log("Updating Source List")
+    zoom.update_sources()
+    sources = obs.obs_enum_sources()
+    log(f"System: {sys}")
+    if sources is not None:
+        obs.obs_property_list_clear(list_property)
+        obs.obs_property_list_add_string(list_property, "", "")
+        for source in sources:
+            if sys == "Darwin":
+                log(f"{obs.obs_source_get_name(source)} | {source}")
+            # Print this value if a source isn't showing in the UI as expected
+            # and add it to SOURCES above for either window or monitor capture.
+            source_type = obs.obs_source_get_id(source)
+            if source_type in SOURCES.all_sources():
+                name_val = name = obs.obs_source_get_name(source)
+                obs.obs_property_list_add_string(list_property, name_val, name)
+        zoom.source_load = True
+    obs.source_list_release(sources)
+    new_source = True
+    log(f"New source: {str(new_source)}")
+
 
 # -------------------------------------------------------------------
 def script_description():
@@ -173,17 +255,44 @@ def script_defaults(settings):
 
 
 def script_update(settings):
-    log("Run script_updates")
+    log("Run script_update")
+    ZoomSettings.save(zs, settings)
 
 
 def script_properties():
     log("Run script_properties")
 
+    global props
+    props = obs.obs_properties_create()
+
+    sources = obs.obs_properties_add_list(
+        props,
+        "source",
+        "Zoom Source",
+        obs.OBS_COMBO_TYPE_LIST,
+        obs.OBS_COMBO_FORMAT_STRING,
+    )
+
+    populate_list_property_with_source_names(sources)
+
+    return props
+
 
 def script_load(settings):
     log("Run script_load")
 
-    settings = zs.load()
+    setting_pairs = {
+        "source": "source_name",
+    }
+
+    settings_import = zs.load()
+
+    for setting in settings_import.keys():
+        if setting in setting_pairs.keys():
+            match = setting_pairs.get(setting)
+            if match in dir(zoom):
+                setattr(zoom, match, settings_import[setting])
+    
 
 
 def script_unload():
